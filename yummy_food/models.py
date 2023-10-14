c71071aeecf69c57132099be614eacc27705172f
Saleha-m14 <>
2023-10-14 22:11:41 +0000

    create Booking model

--- yummy_food/models.py
+++ yummy_food/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create a model for table booking
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    phone = models.IntegerField(max_length=15, null=False, blank=False)
+    email = models.EmailField()
+    person_number = models.IntegerField(null=False, blank=False, default=2)
+    time = models.TimeField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
