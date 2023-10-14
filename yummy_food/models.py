0279aab8ad92efd139a4a3b276b3f0a60913d8bf
Saleha-m14 <>
2023-10-14 23:26:53 +0000

    Deployment commit

--- yummy_food/models.py
+++ yummy_food/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Booking(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    phone = models.IntegerField(max_length=15, null=False, blank=False)
+    phone = models.IntegerField(null=False, blank=False)
     email = models.EmailField()
     person_number = models.IntegerField(null=False, blank=False, default=2)
     time = models.TimeField()
--- yummy_restaurant/settings.py
+++ yummy_restaurant/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 from pathlib import Path
+import os
+
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+pc-bv&x@6lwu+_0s$eyj0x1lv_5&=7x!mx3zv-q%+q!qw=34b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,11 +80,16 @@ WSGI_APPLICATION = 'yummy_restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
