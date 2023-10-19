966d67b2ef4e5db1fe9ba68d03a1202f77a941f3
Saleha-m14 <>
2023-10-19 17:25:21 +0000

    Change booking model

--- yummy_food/models.py
+++ yummy_food/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Booking(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField()
-    phone = models.IntegerField(null=False, blank=False)
+    phone = models.CharField(max_length=50, null=False, blank=False)
     date = models.DateField()
     time = models.TimeField()
     person_number = models.IntegerField(null=False, blank=False, default=2)
